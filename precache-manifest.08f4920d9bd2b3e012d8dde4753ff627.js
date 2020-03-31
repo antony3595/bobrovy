d71323b6f07b3d7c1bbd95a526d1a5d9099688f6
Anton Yatchenko <antony3595@gmail.com>
2020-04-01 00:02:38 +0600

    Updates

---
+++ precache-manifest.08f4920d9bd2b3e012d8dde4753ff627.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe823fd5f070f043c11fc3f807849957",
+    "url": "/bobrovy/index.html"
+  },
+  {
+    "revision": "0eb97c6f70d9f50c2844",
+    "url": "/bobrovy/static/css/main.e111807e.chunk.css"
+  },
+  {
+    "revision": "ea3a7e898e5ecd406cbd",
+    "url": "/bobrovy/static/js/2.7695b9a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bobrovy/static/js/2.7695b9a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb97c6f70d9f50c2844",
+    "url": "/bobrovy/static/js/main.2483ee02.chunk.js"
+  },
+  {
+    "revision": "61a791fbb77888f1c593",
+    "url": "/bobrovy/static/js/runtime-main.e27714d0.js"
+  },
+  {
+    "revision": "7e62413901b5052016b72e7d8c992379",
+    "url": "/bobrovy/static/media/bobrov_alone.7e624139.jpg"
+  },
+  {
+    "revision": "7b0df710e2bf97c0bc68c584ec567296",
+    "url": "/bobrovy/static/media/bobrov_happy.7b0df710.jpg"
+  },
+  {
+    "revision": "3368ba0133331dd9fd58c7d490e038d2",
+    "url": "/bobrovy/static/media/font.3368ba01.ttf"
+  },
+  {
+    "revision": "d56ecf4827d0968f3234cda04f8ccca3",
+    "url": "/bobrovy/static/media/heart.d56ecf48.png"
+  }
+]);
